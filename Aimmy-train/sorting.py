ffc079a320ee469168b3d5acc2426dfe18e48fdf
Le107 <157118445+Le107@users.noreply.github.com>
2025-02-08 15:13:56 +0300

    Add files via upload

---
+++ Aimmy-train/sorting.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+from pathlib import Path
+import numpy as np
+
+os.chdir('images_for_sort')
+
+lst=[1, 2]
+for item in Path('.').glob('*.jpg'):
+    name = str(item)
+    res = name.rstrip('.jpg')
+    r = np.random.choice(lst,1,p=[0.70, 0.30])
+    if r == 1:
+        shutil.move(name, '../dataset/images/train/')
+        print('../dataset/images/train/'+name, '  train')
+        try:
+            shutil.move('../labels_for_sort/'+res+'.txt', '../dataset/labels/train/')
+            print('../dataset/labels/train/'+res+'.txt', '  train')
+        except:
+            print('no label file')
+            pass
+    else:
+        shutil.move(name, '../dataset/images/val/')
+        print('../dataset/images/val/'+name, '  val')
+        try:
+            shutil.move('../labels_for_sort/'+res+'.txt', '../dataset/labels/val/')
+            print('../dataset/labels/val/'+res+'.txt', '  val')
+        except:
+            print('no label file')
+            pass
+        
